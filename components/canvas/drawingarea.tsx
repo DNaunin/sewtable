4bf6234c75b221a534b281e5ecb349d8ae0809de
DNaunin <d.naunin@gmail.com>
2021-03-23 09:41:41 +0100

    ♻️ Renamed and shortened Code for better style

--- components/canvas/drawingarea.tsx
+++ components/canvas/drawingarea.tsx
@@ -1,16 +1,15 @@
 import styles from "./Drawing.module.css";
-import { useEffect } from "react";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function Drawing() {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const ctx = canvasRef.current.getContext("2d");
+    const context = canvasRef.current.getContext("2d");
     const image = new Image();
     image.src = "/pattern_hoodie.svg";
     image.onload = () => {
-      ctx.drawImage(image, 0, 0);
+      context.drawImage(image, 0, 0);
     };
   }, []);
 
