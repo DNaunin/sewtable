5855775b70cb9d9f05368b1b1c3179b0370b3526
DNaunin <d.naunin@gmail.com>
2021-03-24 09:27:58 +0100

    🎨 Added the meta viewport to all pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Sewtable</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- pages/introduction.tsx
+++ pages/introduction.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Sewtable</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={styles.main}>
         <HowTo />
