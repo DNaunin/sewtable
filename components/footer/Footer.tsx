321d84dc83277a2a880370a72d7f3916c32761e8
DNaunin <d.naunin@gmail.com>
2021-04-02 16:27:41 +0200

    ✨ Use Pagelink instead of Buttons in Footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,16 +1,14 @@
 import styles from "./Footer.module.css";
-import Button from "../button/Button";
-import { useRouter } from "next/router";
-import Link from "next/link";
+import Pagelink from "../pagelink/Pagelink";
+// import { useRouter } from "next/router";
 
 export default function Footer() {
-  const router = useRouter();
+  // const router = useRouter();
+
   return (
     <div className={styles.footerContainer}>
-      <Link href={`./`}>
-        <Button label="Home" />
-      </Link>
-      <Button onClick={router.back} label="Back" />
+      <Pagelink href="./">Home</Pagelink>
+      <Pagelink href="">Back</Pagelink>
     </div>
   );
 }
