8737f56f5edf453d90aa917b6214e3bac84e3534
DNaunin <d.naunin@gmail.com>
2021-04-04 10:15:51 +0200

    ✨ Correct the design making new Buttons fit

--- components/canvas/drawingarea.tsx
+++ components/canvas/drawingarea.tsx
@@ -1,36 +1,56 @@
 import styles from "./Drawing.module.css";
 import { useEffect, useRef, useState } from "react";
+import Button from "../button/Button";
+
+let x = 1.0;
+let y = 1.0;
 
 function Drawing() {
   const canvasRef = useRef(null);
   const canvasRef2 = useRef(null);
   const [patternImage, setPatternImage] = useState("");
 
+  function minusClick() {
+    console.log(patternImage);
+    x = Number(x) * 1.08;
+    y = Number(y) * 1.08;
+    console.log(x, y);
+    createPattern(patternImage);
+  }
+
+  function plusClick() {
+    console.log(patternImage);
+    x = Number(x) * 0.8;
+    y = Number(y) * 0.8;
+    console.log(x, y);
+    createPattern(patternImage);
+  }
+
   useEffect(() => {
     setPatternImage(localStorage.getItem("Image"));
   }, []);
 
-  useEffect(() => {
-    const context = canvasRef.current.getContext("2d");
-    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    const context2 = canvasRef.current.getContext("2d");
-    context2.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  }, []);
-
   function createPattern(src) {
     const context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, 302, 152);
     const image = new Image();
     image.src = src;
-    image.onload = () => {
-      const pattern = context.createPattern(image, "repeat");
-      context.fillStyle = pattern;
-      context.fillRect(0, 0, 300, 300);
-    };
+    context.drawImage(
+      image,
+      0,
+      0,
+      image.width * x,
+      image.height * y,
+      0,
+      0,
+      image.width,
+      image.height
+    );
   }
 
   function chooseDesign(src) {
-    const context2 = canvasRef.current.getContext("2d");
-
+    const context2 = canvasRef2.current.getContext("2d");
+    context2.clearRect(0, 0, 302, 152);
     const design = new Image();
     design.src = src;
     design.onload = () => {
@@ -41,23 +61,10 @@ function Drawing() {
   return (
     <div className={styles.patternContainer}>
       <button
-        className={styles.patternimg1}
+        className={styles.patternimg}
         onClick={() => createPattern(patternImage)}
       >
-        <img className={styles.patternimg1} src={patternImage} />
-      </button>
-      <button
-        className={styles.patternimg2}
-        onClick={() =>
-          createPattern(
-            "https://images.unsplash.com/photo-1563989968497-0c52b4093d39?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-          )
-        }
-      >
-        <img
-          className={styles.patternimg2}
-          src="https://images.unsplash.com/photo-1563989968497-0c52b4093d39?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-        />
+        <img className={styles.patternimg} src={patternImage} />
       </button>
       <canvas ref={canvasRef} className={styles.canvas1}></canvas>
       <canvas ref={canvasRef2} className={styles.canvas2}></canvas>
@@ -73,6 +80,8 @@ function Drawing() {
       >
         <img className={styles.designbtn2} src="/pulliSven.png" />
       </button>
+      <Button className={styles.zoombtn} label="-" onClick={minusClick} />
+      <Button className={styles.zoombtn} label="+" onClick={plusClick} />
     </div>
   );
 }
