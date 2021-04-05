3c93fb2022e8343a69436be41f3c8efd63b1f37c
DNaunin <d.naunin@gmail.com>
2021-04-05 19:04:56 +0200

    ✨ Integrate example Pattern Picture as Button. Function can now be tested without uploading a pattern picture

--- components/canvas/drawingarea.tsx
+++ components/canvas/drawingarea.tsx
@@ -10,21 +10,18 @@ function Drawing() {
   const canvasRefFabric = useRef(null);
   const canvasRefDesign = useRef(null);
   const [patternImage, setPatternImage] = useState("");
+  const [chosenPattern, setChosenPattern] = useState("");
 
   function minusClick() {
-    console.log(patternImage);
     x = Number(x) * 1.08;
     y = Number(y) * 1.08;
-    console.log(x, y);
-    createPattern(patternImage);
+    createPattern(chosenPattern);
   }
 
   function plusClick() {
-    console.log(patternImage);
     x = Number(x) * 0.8;
     y = Number(y) * 0.8;
-    console.log(x, y);
-    createPattern(patternImage);
+    createPattern(chosenPattern);
   }
 
   useEffect(() => {
@@ -60,19 +57,36 @@ function Drawing() {
     };
   }
 
+  function choosePattern(src) {
+    setChosenPattern(src);
+    resetZoom();
+    createPattern(chosenPattern);
+  }
+
+  function resetZoom() {
+    x = 1.0;
+    y = 1.0;
+  }
+
   return (
     <div className={styles.patternContainer}>
       <canvas ref={canvasRefFabric} className={styles.canvasFabric}></canvas>
       <canvas ref={canvasRefDesign} className={styles.canvasDesign}></canvas>
       <button
         className={styles.patternimg}
-        onClick={() => createPattern(patternImage)}
+        onClick={() => choosePattern(patternImage)}
       >
         <img className={styles.patternimg} src={patternImage} />
       </button>
       <Button className={styles.minusbtn} label="-" onClick={minusClick} />
       <Button className={styles.plusbtn} label="+" onClick={plusClick} />
       <Pagelink href="/design">Different Design</Pagelink>
+      <button
+        className={styles.examplePattern}
+        onClick={() => choosePattern("/pusteblume.jpg")}
+      >
+        <img className={styles.examplePattern} src={"/pusteblume.jpg"} />
+      </button>
     </div>
   );
 }
