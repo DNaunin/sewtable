0dee13522fec647d973a41dbb40a5f01b9a13020
DNaunin <d.naunin@gmail.com>
2021-04-05 17:16:29 +0200

    ✨ Insert punctuation

--- components/messages/HowTo.tsx
+++ components/messages/HowTo.tsx
@@ -6,9 +6,9 @@ function HowTo() {
     <div className={styles.howToContainer}>
       <span className={styles.title}>How it works</span>
       <ul className={styles.info}>
-        <li>Choose one of the offered sewing patterns</li>
+        <li>Choose one of the offered sewing patterns.</li>
         <li>Take a picture of your favourite fabric.</li>
-        <li>Upload it to the cloud</li>
+        <li>Upload it to the cloud.</li>
         <li> Scale your pattern on the chosen design.</li>
         <li>How does it look?</li>
       </ul>
