71196db627273fac770ebaec2923fb9d3ca27770
DNaunin <d.naunin@gmail.com>
2021-04-07 19:35:42 +0200

    🎨  Rename Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Sewtable</title>
-        <link rel="icon" href="/Sewing.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
   );
