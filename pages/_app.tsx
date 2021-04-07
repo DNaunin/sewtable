5a0f4224518d98abe09c5e35f8c06bac7a370759
DNaunin <d.naunin@gmail.com>
2021-04-07 11:04:50 +0200

    ✨ Add Head to app.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Sewtable</title>
+        <link rel="icon" href="/Sewing.ico" />
+      </Head>
+    </>
+  );
 }
 
 export default MyApp;
