6116dc4fa724aea3edc9c2c033d7d48b7f2513d2
DNaunin <d.naunin@gmail.com>
2021-04-07 11:05:08 +0200

    ✨ Delete Head from all pages

--- pages/canvas.tsx
+++ pages/canvas.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import styles from "../styles/Canvas.module.css";
 import Drawing from "../components/canvas/drawingarea";
 import Footer from "../components/footer/Footer";
@@ -6,12 +5,6 @@ import Footer from "../components/footer/Footer";
 export default function Canvas() {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Sewtable</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
         <span className={styles.title}>Combine fabric and pattern </span>
         <Drawing />
