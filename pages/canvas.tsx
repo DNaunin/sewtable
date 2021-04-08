ac90d3491712b17df071008bccb73abcc7227e01
DNaunin <d.naunin@gmail.com>
2021-04-08 13:55:12 +0200

    ✨ Draw everything on the same canvas

--- pages/canvas.tsx
+++ pages/canvas.tsx
@@ -1,13 +1,16 @@
 import styles from "../styles/Canvas.module.css";
-import Drawing from "../components/canvas/drawingarea";
 import Footer from "../components/footer/Footer";
-
+import dynamic from "next/dynamic";
+const DynamicDrawing = dynamic(
+  () => import("../components/canvas/drawingarea"),
+  { ssr: false }
+);
 export default function Canvas() {
   return (
     <>
       <main className={styles.main}>
         <span className={styles.title}>Combine fabric and pattern </span>
-        <Drawing />
+        <DynamicDrawing />
         <Footer />
       </main>
     </>
