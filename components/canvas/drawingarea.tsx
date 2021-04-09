5926964e9b7ca684d7bf55b35e955d05d0eda7df
DNaunin <d.naunin@gmail.com>
2021-04-09 14:46:52 +0200

    ✨ Save Canvas as png and in localStorage

--- components/canvas/drawingarea.tsx
+++ components/canvas/drawingarea.tsx
@@ -2,44 +2,40 @@ import styles from "./Drawing.module.css";
 import { useEffect, useRef, useState } from "react";
 import Button from "../button/Button";
 import PageLink from "../pagelink/Pagelink";
+import { useRouter } from "next/router";
 
 function getFromLocalStorage<T>(key: string, defaultValue: T) {
   return localStorage.getItem(key) || defaultValue;
 }
-
 function Drawing() {
+  const router = useRouter();
   const [zoom, setZoom] = useState<number>(1.0);
   const canvasRefDesign = useRef<HTMLCanvasElement>(null);
   const uploadedImageSrc = getFromLocalStorage<string>(
     "Image",
     "/pusteblume.jpg"
   );
-
   const selectedPatternSrc = getFromLocalStorage<string>("Design", "");
-  const selectedPattern = new Image();
-  selectedPattern.src = selectedPatternSrc;
   useEffect(() => {
-    const context = canvasRefDesign.current.getContext("2d");
+    const designCanvas = canvasRefDesign.current;
+    const context = designCanvas.getContext("2d");
     const uploadedImage = new Image();
+    uploadedImage.crossOrigin = "anonymous";
     uploadedImage.src = uploadedImageSrc;
-
-    const myCanvas = canvasRefDesign.current;
-    const canvasImage = myCanvas.toDataURL();
-    console.log(canvasImage);
-
-    context.clearRect(0, 0, selectedPattern.width, selectedPattern.height);
-
-    function drawSelectedPattern() {
-      context.drawImage(
-        selectedPattern,
-        0,
-        0,
-        selectedPattern.width,
-
-        selectedPattern.height
-      );
+    const selectedPattern = new Image();
+    selectedPattern.crossOrigin = "anonymous";
+    selectedPattern.src = selectedPatternSrc;
+    function handleSelectedPatternLoad() {
+      canvasRefDesign.current.width = selectedPattern.width;
+      canvasRefDesign.current.height = selectedPattern.height;
+      context.clearRect(0, 0, selectedPattern.width, selectedPattern.height);
+      if (uploadedImage.complete) {
+        handleUploadedImageLoad();
+      } else {
+        uploadedImage.onload = handleUploadedImageLoad;
+      }
     }
-    function drawUploadedImage() {
+    function handleUploadedImageLoad() {
       context.drawImage(
         uploadedImage,
         0,
@@ -51,19 +47,20 @@ function Drawing() {
         uploadedImage.width * zoom,
         uploadedImage.height * zoom
       );
-      if (selectedPattern.complete) {
-        drawSelectedPattern();
-      } else {
-        selectedPattern.onload = drawSelectedPattern;
-      }
+      context.drawImage(
+        selectedPattern,
+        0,
+        0,
+        selectedPattern.width,
+        selectedPattern.height
+      );
     }
-    if (uploadedImage.complete) {
-      drawUploadedImage();
+    if (selectedPattern.complete) {
+      handleSelectedPatternLoad();
     } else {
-      uploadedImage.onload = drawUploadedImage;
+      selectedPattern.onload = handleSelectedPatternLoad;
     }
   }, [uploadedImageSrc, selectedPatternSrc, zoom]);
-
   function minusClick() {
     const uploadedImage = new Image();
     uploadedImage.src = uploadedImageSrc;
@@ -75,22 +72,24 @@ function Drawing() {
     setZoom(+zoom * 1.1);
   }
 
+  function saveCanvas() {
+    const designCanvas = canvasRefDesign.current;
+    const canvasImage = designCanvas.toDataURL();
+    localStorage.setItem("canvasImage", canvasImage);
+    router.push("/final");
+  }
+
   return (
     <div className={styles.container}>
-      <canvas
-        ref={canvasRefDesign}
-        className={styles.canvasDesign}
-        height={selectedPattern.height}
-        width={selectedPattern.width}
-      />
+      <canvas ref={canvasRefDesign} className={styles.canvasDesign} />
       <button className={styles.patternImg}>
         <img className={styles.patternImg} src={uploadedImageSrc} />
       </button>
       <Button className={styles.minusBtn} label="-" onClick={minusClick} />
       <Button className={styles.plusBtn} label="+" onClick={plusClick} />
       <PageLink href="/design">Different Design</PageLink>
+      <Button onClick={saveCanvas} label="Show Final Product" />
     </div>
   );
 }
-
 export default Drawing;
